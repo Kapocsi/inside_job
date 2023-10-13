6dde704da2e53da61d1870f2744292be0c8f42a5
Dylan Borchert <dylanbor01@gmail.com>
2023-10-13 13:15:00 -0600

    added map kinda

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from 'svelte';
+    const Leaflet = import('leaflet');
+    let map;
+
+    onMount(async () => {
+        const L = await Leaflet;
+        map = L.map('map').setView([51.505, -0.09], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+        }).addTo(map);
+    });
+
+
+
+</script>
+
+<div id="map">test</div>
