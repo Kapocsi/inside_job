37e98a0ae4ac022fb329e15ace4dc508d0e04e0a
Ezzidean <65983076+Ezzidean@users.noreply.github.com>
2023-10-13 19:17:07 +0000

    attempted to create event handler for clicking

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,33 +1,49 @@
+
+
 <script>
     let events = [
       {
         title: 'Sample Event 1',
         date: '2023-10-13',
         description: 'This is the first sample event description.',
+        focused: false
       },
       {
         title: 'Sample Event 2',
         date: '2023-10-14',
         description: 'This is the second sample event description.',
+        focused: false
       },
       {
         title: 'Sample Event 3',
         date: '2023-10-15',
         description: 'This is the third sample event description.',
+        focused: false,
       } ]
   
+    /**
+	 * @param {{ title: string; date: string; description: string; }} events
+	 */
     function handleEventClick(events) {
-      console.log('Event clicked:', events);
       // You can add logic to handle the click for the specific event here
     }
+
+
+
   </script>
   
   {#each events as event (event.title)}
+    
+    {#if event.focused}
+    hello
+    {:else}
     <div class="event" on:click={() => handleEventClick(event)}>
-      <h3>{event.title}</h3>
-      <p>Date: {event.date}</p>
-      <p>Description: {event.description}</p>
+    <h3>{event.title}</h3>
+    <p>Date: {event.date}</p>
+    <p>Description: {event.description}</p>
     </div>
+    {/if}
+
   {/each}
   
   <style>
@@ -41,8 +57,14 @@
       transition: background-color 0.2s;
       max-width: 20%;
     }
-  
+    
     .event:hover {
       background-color: #e0e0e0;
     }
+
+    #focus {
+        max-width: 60%;
+        max-height: 60%;
+    }
+    
   </style>
