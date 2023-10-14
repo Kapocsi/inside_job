bd5948ce82acf16f9d599aa9c746a3a0a32e6976
Dylan Borchert <dylanbor01@gmail.com>
2023-10-13 23:37:32 -0600

    go button

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import CssRest from '../reset.css.svelte';
+
+    
+
 </script>
 
 
@@ -15,6 +18,10 @@
         <select>
             <option>Demo</option>
         </select>
+
+        <div class="button">
+            <button>Go</button>
+        </div>
     </div>
 </div>
 
@@ -26,6 +33,7 @@
         align-items: center;
         justify-content: center;
         height: 100vh;
+        position: relative;
         /* madefrom chat gpt */
         background:
                 linear-gradient(0deg, #f0f0f0 1px, transparent 1px),
@@ -34,10 +42,14 @@
     }
 
     .shadow2 {
+        position: absolute;
+        z-index: -1;
         box-shadow: -150px -50px 200px 200px rgba(80, 180, 200, 0.5);
     }
 
     .shadow1 {
+        position: absolute;
+        z-index: -1;
         box-shadow: 150px 50px 200px 200px rgba(30, 43, 200, 0.5)
     }
 
@@ -45,12 +57,30 @@
         width: 100%;
         height: 25px;
         border-radius: 0.5rem;
-
     }
 
     .title {
         font-size: 3rem;
         font-weight: 900;
+        padding: 1.5rem 0;
+    }
+
+
+    .button {
+        width: 100%;
+        padding: 1rem 0;
+        display: flex;
+    }
+    button {
+        width: auto;
+        height: 2rem;
+        padding: 0 1rem;
+        margin: auto;
+        border-radius: 0.5rem;
+        background: #f0f0f0;
+        border: none;
+        font-size: 1rem;
+        cursor: pointer;
     }
 
 </style>
